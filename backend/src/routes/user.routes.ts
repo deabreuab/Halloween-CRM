a323e0b58761006d8ba8a236a48f3ee3a336130c
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-10-28 16:58:08 -0600

    chore: middleware function

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import express, { Router } from 'express';
 import { createUser, getUser, getUserById, loginUser, updateUser, deleteUser } from '../controllers/user.controller';
+import { isAdmin } from '../middleware/auth.middleware';
 
 const routerUser: Router = express.Router();
 
-// const { isAdmin } = require('../middleware/auth.middleware');
 
 routerUser.post('/', createUser);
-routerUser.get('/',  getUser);
+routerUser.get('/', isAdmin,  getUser);
 routerUser.get('/:id', getUserById);
 routerUser.post('/login', loginUser);
 routerUser.put('/:id', updateUser);
