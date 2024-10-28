4f0b0e578588ca586257b254b865af8fede1dd42
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-10-28 13:58:15 -0500

    style: agregar estilos a colaboradores

--- Halloween-CRM/src/components/Layout.tsx
+++ Halloween-CRM/src/components/Layout.tsx
@@ -1,14 +1,17 @@
 import { Box } from "@mui/material";
 import Sidebar from "./SideBar";
+import TopBar from "./TopBar";
+import { ReactNode } from "react";
 interface LayoutProps {
   children: ReactNode;
 }
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex", height: "100vh" }}>
       <Sidebar />
       <Box sx={{ flexGrow: 1 }}>
-        <Box component="main" sx={{ padding: 3 }}>
+        <TopBar />
+        <Box component="main" sx={{ padding: 3, mt: 10 }}>
           {children}
         </Box>
       </Box>
