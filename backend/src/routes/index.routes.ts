26f27cb79d69e9e18f522d9c54603fd6c67fc247
Barbara <0deabreubarbara@gmail.com>
2024-10-28 15:41:04 -0500

    feat: adding opportunities routes to index router

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,10 +1,12 @@
 import express, {Application, Router} from "express";
 import { routerUser } from "./user.routes";
+import { opportunitiesRouter } from "./opportunities.routes";
 
 function router(app:Application): void {
   const routes: Router = express.Router(); 
   app.use('/home', routes);  
   routes.use('/user', routerUser);
+  routes.use('/opportunities', opportunitiesRouter);
 }
 
 export {router};
