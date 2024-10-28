2b69107e2edfdba812d9bf2179b9f58212c5d2dd
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-10-28 12:03:47 -0600

    feat: controller changes

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,28 +1,58 @@
 import { Request, Response } from "express";
 import { Users } from "../models/user.models";
-// import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt'
 
+const encryptedPassword = async (password:string) =>{
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+const comparePassword = async (password:string, passwordHash:string) =>{
+    return await bcrypt.compare(password, passwordHash);
+}
 
-// const encryptedPassword = async (password: string) =>{
-//     const salt = await bcrypt.genSalt(10);
-//     return await bcrypt.hash(password, salt);
-//   }
-//   const comparedPassword = awaitUsers.methods.comparedPassword = async function(password: string){//this hace referencia a userSchema
-//     return await bcrypt.compare(password, this.password);
-//   }
-
-export const createUser = async (req: Request, res: Response) => {
+export const createUser = async (req: Request, resp: Response) => {
     try {
         const { name, email, password, phone } = req.body;
-        // const encryptedPassword = await Users.schema.methods.encryptedPassword(password);
-        // const newUser = new Users({ name, email, password: encryptedPassword, phone });
-        const newUser = new Users({name: name, email:email, password: password, phone: phone})
+        const newPassword = await encryptedPassword(password);
+        const newUser = new Users({name: name, email:email, password: newPassword, phone: phone})
         await newUser.save();
-        console.log("��� ~ createUser ~ newUser:", newUser)
-        res.status(201).json("creado");
+        resp.status(201).json({message:"Colaborador creado", newUser});
     } catch (error) {
-        console.log("🚀 ~ createUser ~ error:", error)
-        res.status(400).json(error);
+        resp.status(500).json(error);
     }
 };
 
+export const loginUser = async (req: Request, resp: Response): Promise<void> => {
+    try {
+        const { email, password } = req.body;
+        resp.status(200).json({message: "Acceso verificado"});
+    } catch (error) {
+        resp.status(500).json({message: "Error del servidor"})
+    }
+}
+
+
+export const getUser = async (req: Request, resp: Response) => {
+    try {
+        const findUser = await Users.find()
+
+        resp.status(200).json({message: "Usuarios obtenidos", findUser});
+    } catch (error) {
+        resp.status(500).json(error);
+    }
+}
+
+export const getUserById = async (req: Request, resp: Response): Promise<any> => {
+    try {
+        const user = await Users.findById(req.params.id);
+        if (!user) {
+            return resp.status(404).json({ message: "No se encontró el usuario" });
+        }
+    } catch (error) {
+        return resp.status(500).json({ message: "Error del servidor" });
+    }
+}
+
+export const updateUser = async (req: Request, resp: Response) => {}
+
+export const deleteUser = async (req: Request, resp: Response) => {}
--- backend/src/models/user.models.ts
+++ backend/src/models/user.models.ts
@@ -9,6 +9,8 @@ const userSchema = new Schema<IUser, UserModel>({
   photo: { type: String, default: 'default_photo.jpg' },
   phone: { type: String, required: true },
   password: { type: String, required: true },
+  company: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'collaborator'], default: 'collaborator' },
 },{timestamps:true});//SUSTITUYE A CREATEDAT Y UPDATEDAT PARA NO TENER QUE ESTARLOS ACTUALIZANDO
 
 export const Users: UserModel = mongoose.model<IUser, UserModel>('Users', userSchema);//NOMBRE DEL ESQUEMA, Y EL ESQUEMA
