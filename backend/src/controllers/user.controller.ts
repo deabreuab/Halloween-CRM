061a5531b182a78c002c7a5f24d49c519d746385
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-10-28 10:58:08 -0600

    feat: app code line of the index, and controller to create a ready user

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from "express";
+import { Users } from "../models/user.models";
+// import bcrypt from 'bcrypt'
 
-const getUser = (req: Request, res: Response) => {
+
+// const encryptedPassword = async (password: string) =>{
+//     const salt = await bcrypt.genSalt(10);
+//     return await bcrypt.hash(password, salt);
+//   }
+//   const comparedPassword = awaitUsers.methods.comparedPassword = async function(password: string){//this hace referencia a userSchema
+//     return await bcrypt.compare(password, this.password);
+//   }
+
+export const createUser = async (req: Request, res: Response) => {
     try {
-        res.render('')
+        const { name, email, password, phone } = req.body;
+        // const encryptedPassword = await Users.schema.methods.encryptedPassword(password);
+        // const newUser = new Users({ name, email, password: encryptedPassword, phone });
+        const newUser = new Users({name: name, email:email, password: password, phone: phone})
+        await newUser.save();
+        console.log("��� ~ createUser ~ newUser:", newUser)
+        res.status(201).json("creado");
     } catch (error) {
-       res.status(500).json(error); 
+        console.log("🚀 ~ createUser ~ error:", error)
+        res.status(400).json(error);
     }
-}
+};
+
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,7 +1,8 @@
 import express, { Router } from 'express';
+import { createUser } from '../controllers/user.controller';
 
 const routerUser: Router = express.Router();
 
-routerUser.get('/', );
+routerUser.post('/', createUser);
 
 export {routerUser};
