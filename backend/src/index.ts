604abb92123dc5d238ffbe14f97974b0677dc5c3
Aylin <aylin_santacruz@hotmail.com>
2024-10-29 00:21:07 -0500

    feat:Add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 import {connectMongo} from './connection';
+import cors from 'cors'; 
 import { router } from './routes/index.routes';
 import { authMiddleware } from './middleware/auth.middleware';
+
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT || '3000');
+
+app.use(cors());
 app.use(express.json())
 const secret: string = process.env.secret || 'secret';
 app.use(authMiddleware(secret));
