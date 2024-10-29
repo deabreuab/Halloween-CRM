2fdceefa6e3ac1047dba9d370739e215b942d2be
Aylin <aylin_santacruz@hotmail.com>
2024-10-29 00:11:33 -0500

    feat:Add task

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,13 +1,17 @@
 import { Application, Router } from "express";
 import { routerUser } from "./user.routes";
+import { routerParticipant } from "./participant.routes";
+import { opportunitiesRouter } from "./opportunities.routes";
+import { tasksRouter } from "./tasks.routes";
 
 function router(app: Application): void {
-    const routes: Router = Router();
-    app.use('/home', routes);  
+  const routes: Router = Router();
+  app.use('/home', routes);
 
-    routes.use('/user', routerUser);
-    routes.use('/participant', routerParticipant);
+  routes.use('/user', routerUser);
+  routes.use('/participant', routerParticipant);
   routes.use('/opportunities', opportunitiesRouter);
+  routes.use('/tasks', tasksRouter);
 }
 
-export {  router  };
+export { router };
