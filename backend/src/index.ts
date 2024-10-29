e76c4e946360a5673ecd3cd810466ab281478aa2
Aylin <aylin_santacruz@hotmail.com>
2024-10-28 21:06:44 -0500

    fix:Choose backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,20 @@
-
-import connectMongo from './connection';
-import Participant from './models/participant.model';
 import express, { Application, Request, Response } from 'express';
-// import {authMiddleware} from './middleware/auth.middleware';
-
+import {connectMongo} from './connection';
+import { router } from './routes/index.routes';
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT || '8001');
-const secret: string = process.env.SECRET || 'secret';
-// const auth = authMiddleware(secret);
-// app.use(auth);
-
+app.use(express.json())
 router(app);
 
-const app: Application = express();
-const PORT: number = 8000;
+app.get("/", (req: Request, resp: Response) => {
+  resp.send("connection mongodb moongose")
+});
 
 const startServer = async () =>{
-    await connectMongo();
-    const participant = new Participant({
-        email:"rosa@gmail.com",
-        name:"rosa",
-        createdBy:"maria",
-    });
-    await participant.save();
-    console.log(participant)
-    app.listen(PORT, (): void => {
-        console.log('SERVER IS UP ON PORT:', PORT);
-    });
+  await connectMongo();
+  app.listen(PORT, (): void => {
+    console.log('SERVER IS UP ON PORT:', PORT);
+  });
 }
-
 
 startServer();
--- backend/src/models/participant.model.ts
+++ backend/src/models/participant.model.ts
@@ -1,22 +1,16 @@
 import mongoose, { Schema, model, connect } from 'mongoose';
-
-
-interface IParticipant {
-    email:string;
-    name:string;
-    createdBy:string; // creado por (persona)
-    modifiedBy?:string; // modificado por (persona)
-}
-
+import { IParticipant } from '../interface/index.interface';
 
 const  ParticipantSchema = new Schema<IParticipant>({
-    email: { type: String, required: true },
-    name: { type: String, required: true },
-    createdBy:{type:String,required: true},
-    modifiedBy:{type:String}
-},{timestamps:true}) // añade automaticamento createAt y updateAt
+  email: { type: String, required: true },
+  name: { type: String, required: true },
+  //createdAt :{type:Date,default:Date.now},
+  createdBy:{type:String},
+  //modifiedAt:{type:Date,default:Date.now},
+  modifiedBy:{type:String}
+},{timestamps:true})
 
+const Participant = mongoose.model<IParticipant>('Participant', ParticipantSchema);
 
-const Participant = mongoose.model<IParticipant>('Participant',ParticipantSchema);
  
-export default Participant;
+export { Participant };
