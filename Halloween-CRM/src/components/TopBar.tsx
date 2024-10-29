86d4e5e10a212c37e60581d3e03072e3cd8eaa79
Aylin <aylin_santacruz@hotmail.com>
2024-10-29 18:43:43 -0500

    feat:Add logout

--- Halloween-CRM/src/components/TopBar.tsx
+++ Halloween-CRM/src/components/TopBar.tsx
@@ -1,13 +1,27 @@
-import { AppBar, Avatar, IconButton, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  Toolbar,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+} from "@mui/material";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import { useEffect,useState } from "react";
-// import { deepOrange, deepPurple } from "@mui/material/colors";
+import { useEffect, useState } from "react";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { useNavigate } from "react-router-dom";
 
 const TopBar: React.FC = () => {
   const drawerWidth = 240;
   const [userName, setUserName] = useState<string | null>(
     localStorage.getItem("userName")
   );
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const storedUserName = localStorage.getItem("userName");
@@ -19,7 +33,6 @@ const TopBar: React.FC = () => {
     return name.charAt(0).toUpperCase();
   };
 
-
   const generateRandomColor = () => {
     const letters = "0123456789ABCDEF";
     let color = "#";
@@ -28,27 +41,63 @@ const TopBar: React.FC = () => {
     }
     return color;
   };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("userName");
+    setUserName(null);
+    handleClose();
+    navigate("/login"); 
+  };
+
   const initials = getInitials(userName);
   const avatarColor = generateRandomColor();
 
   return (
-    <AppBar
-      position="fixed"
-      sx={{
-        width: `calc(100% - ${drawerWidth}px)`,
-        ml: `${drawerWidth}px`,
-        backgroundColor: "#467E91",
-      }}
-    >
-      <Toolbar
-        sx={{ display: "flex", justifyContent: "flex-end", flexGrow: 1 }}
+    <>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: `calc(100% - ${drawerWidth}px)`,
+          ml: `${drawerWidth}px`,
+          backgroundColor: "#467E91",
+        }}
       >
-        <IconButton color="inherit">
-          <NotificationsNoneIcon />
-        </IconButton>
-        <Avatar sx={{ bgcolor: avatarColor }}>{initials}</Avatar>
-      </Toolbar>
-    </AppBar>
+        <Toolbar
+          sx={{ display: "flex", justifyContent: "flex-end", flexGrow: 1 }}
+        >
+          <IconButton color="inherit">
+            <NotificationsNoneIcon />
+          </IconButton>
+          <Avatar sx={{ bgcolor: avatarColor }} onClick={handleClickOpen}>
+            {initials}
+          </Avatar>
+        </Toolbar>
+      </AppBar>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Cerrar Sesión</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            ¿Estás seguro de que deseas cerrar sesión?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleLogout} color="secondary" autoFocus>
+            <ExitToAppIcon sx={{ marginRight: 1 }} /> Cerrar Sesión
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 export default TopBar;
